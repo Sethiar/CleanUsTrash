e14c69896cb317d8747207361714833b34ffcaba
Lefetey Arnaud <alefetey123@gmail.com>
2025-01-02 21:03:29 +0100

    Mise en place de la logique de navigation.

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,10 +4,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import HomePage from '../components/screens/HomePage';
-import AuthPage from '../components/screens/AuthPage';
-import LoginPage from '../components/screens/LoginPage';
-import SignUpPage from '../components/screens/SignUpPage';
-
+import AuthPage from '../components/screens/LogDomain/AuthPage';
+import LoginPage from '../components/screens/LogDomain/LoginPage';
+import SignUpPage from '../components/screens/LogDomain/SignUpPage';
+import SignInSuccess from '../components/screens/LogDomain/SignInSuccess';
+import SignUpSuccess from '../components/screens/LogDomain/SignUpSuccess';
+import MainPage from '../components/screens/MainPages/MainPage';
+import MainProcess1 from '../components/screens/MainPages/Process1/MainProcess1';
+import MainProcess2 from '../components/screens/MainPages/Process2/MainProcess2';
+import MainProcess3 from '../components/screens/MainPages/Process3/MainProcess3';
 const Stack = createStackNavigator();
 
 const AppNavigator = () => (
@@ -17,6 +22,12 @@ const AppNavigator = () => (
       <Stack.Screen name="Auth" component={AuthPage} />
       <Stack.Screen name="Login" component={LoginPage} />
       <Stack.Screen name="SignUp" component={SignUpPage} />
+      <Stack.Screen name="SignInSuccess" component={SignInSuccess} />
+      <Stack.Screen name="SignUpSuccess" component={SignUpSuccess} />
+      <Stack.Screen name="MainPage" component={MainPage} />
+      <Stack.Screen name="MainProcess1" component={MainProcess1} />
+      <Stack.Screen name="MainProcess2" component={MainProcess2} />
+      <Stack.Screen name="MainProcess3" component={MainProcess3} />
     </Stack.Navigator>
   </NavigationContainer>
 );
