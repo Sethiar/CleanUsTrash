febaf260d86da430bb56b4d2f3a1dac5e23ff8ba
Lefetey Arnaud <alefetey123@gmail.com>
2025-01-02 21:00:08 +0100

    Mise à jour des routes de navigaiton.

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,11 +1,47 @@
+//Lien de navigation de l'application
+
+/* Navigation vers les options d'authentification */
 export const goToAuthPage = (navigation) => {
   navigation.navigate('Auth');
 };
 
+/* Navigation vers la page de connexion */
 export const goToLoginPage = (navigation) => {
   navigation.navigate('Login');
 };
 
+/* Navigation vers la page succès de connexion */
+export const goToSignInSuccess = (navigation) => {
+  navigation.navigate('SignInSuccess');
+};
+
+/* Navigation vers la page d'inscription */
 export const goToSignUpPage = (navigation) => {
   navigation.navigate('SignUp');
 };
+
+/* Navigation vers la page succès d'inscription */
+export const goToSignUpSuccess = (navigation) => {
+  navigation.navigate('SignUpSuccess');
+};
+
+/* Navigation vers la page principale de l'application */
+export const goToMainPage = (navigation) => {
+  navigation.navigate('MainPage');
+};
+
+/* Navigation vers la page principale du processus 1 */
+export const goToMainProcess1 = (navigation) => {
+  navigation.navigate('MainProcess1')
+};
+
+/* Navigation vers la page principale du processus 2 */
+export const goToMainProcess2 = (navigation) => {
+  navigation.navigate('MainProcess2')
+};
+
+/* Navigation vers la page principale du processus 3*/
+export const goToMainProcess3 = (navigation) => {
+  navigation.navigate('MainProcess3')
+};
+
